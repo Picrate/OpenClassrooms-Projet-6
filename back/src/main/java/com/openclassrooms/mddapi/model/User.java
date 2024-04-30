ff09b61121e275c6d5629a649a1f82fd70e125f2
Patrice ALLARY <dev@allary-guillery.fr>
2024-04-30 08:33:23 +0200

    Basic User structure & implementation

---
+++ back/src/main/java/com/openclassrooms/mddapi/model/User.java
@@ -0,0 +1,18 @@
+package com.openclassrooms.mddapi.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Getter
+@Setter
+@Document(collection = "users")
+public class User {
+
+    @Id
+    private String id;
+    private String username;
+    private String password;
+    private String email;
+}
---
+++ back/src/main/java/com/openclassrooms/mddapi/service/UserService.java
@@ -0,0 +1,24 @@
+package com.openclassrooms.mddapi.service;
+
+import com.openclassrooms.mddapi.model.User;
+import com.openclassrooms.mddapi.repository.UserRepository;
+
+import java.util.Optional;
+
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+    public User getUserByEmail(String email) {
+        Optional<User> user = userRepository.findByEmail(email);
+        if (user.isPresent()) {
+            return user.get();
+        } else {
+            return null;
+        }
+    }
+
+}
