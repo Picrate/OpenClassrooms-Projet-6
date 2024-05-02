57fd59d8958318b369e5f144c5e001f5252ab8fd
Patrice ALLARY <dev@allary-guillery.fr>
2024-05-02 16:51:53 +0200

    Add Swagger Api documentation

--- back/src/main/java/com/openclassrooms/mddapi/MddApiApplication.java
+++ back/src/main/java/com/openclassrooms/mddapi/MddApiApplication.java
@@ -2,6 +2,7 @@ package com.openclassrooms.mddapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 public class MddApiApplication {
