2a205ed2a2a176965cedcb6387190d21d83b6b55
Patrice ALLARY <dev@allary-guillery.fr>
2024-05-02 16:02:35 +0200

    User Attributes redefinition

--- back/src/main/java/com/openclassrooms/mddapi/mapper/UserMapper.java
+++ back/src/main/java/com/openclassrooms/mddapi/mapper/UserMapper.java
@@ -1,11 +1,16 @@
 package com.openclassrooms.mddapi.mapper;
 
 import com.openclassrooms.mddapi.dto.UserDto;
+import com.openclassrooms.mddapi.dto.SimpleUserDto;
 import com.openclassrooms.mddapi.model.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface UserMapper {
+    @Mapping(target = "created_at", source = "createdAt")
+    @Mapping(target = "updated_at", source = "updatedAt")
     UserDto userToDto(User user);
     User userDtoToUser(UserDto userDto);
+    SimpleUserDto userToSimpleUser(User user);
 }
--- back/src/main/java/com/openclassrooms/mddapi/model/User.java
+++ back/src/main/java/com/openclassrooms/mddapi/model/User.java
@@ -4,7 +4,9 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,6 +21,10 @@ public class User {
     private String password;
     private String email;
     private List<String> topics;
+    @Field("created_at")
+    private String createdAt;
+    @Field("updated_at")
+    private String updatedAt;
 
     public User() {
         this.topics = new ArrayList<>();
